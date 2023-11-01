a79ca5ce22c42bf9b1feeda547ce8d7db8def19e
Abu Zakaria <asm.zakaria120@gmail.com>
2023-11-02 00:46:03 +0600

    update

--- device1.go
+++ device1.go
@@ -6,7 +6,8 @@ import (
 )
 
 func connect() {
-	friend_address := "0.0.0.0:6969"
+	friend_address := "103.121.104.177:6969"
+
 	conn, err := net.Dial("tcp", friend_address)
 	if err != nil {
 		panic("couldn't connect with " + friend_address)
